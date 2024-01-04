86b57abe773188def27edb02262632904910a394
Roark McKibben <93952263+Djtpj@users.noreply.github.com>
2024-01-03 22:19:25 -0600

    Tweaked values

--- basketball_goal_code/basketball_goal_code.ino
+++ basketball_goal_code/basketball_goal_code.ino
@@ -5,7 +5,7 @@ const int CLOCK = 4;
 const int SIGNAL = 3;
 
 // Delay between each update of the number on the display
-const int TICK_SPEED_SECONDS = 2;
+const float TICK_SPEED_SECONDS = 0.75;
 
 TM1637Display display = TM1637Display(CLOCK, SIGNAL);
 
@@ -17,7 +17,7 @@ void setup() {
 
 void loop() {
   // We only want to generate four digit numbers because that's how many the display can support. No more, no less
-  const int MAX_VAL = 9999, MIN_VAL = 1000;
+  const int MAX_VAL = 99, MIN_VAL = 0;
   display.showNumberDec(random(MIN_VAL, MAX_VAL));
 
   delay(TICK_SPEED_SECONDS * 1000);
